2c2854ef423421385a8c70f458ae03420005939a
ranjeetindelhi <ranjeetindelhi@gmail.com>
2025-02-28 14:19:32 +0530

    Class file added for Team A

---
+++ TeamA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub.Tutorial.CodingTracker
+{
+    public class TeamA
+    {
+        public int TeamId { get; set; }
+        public string TeamDescription { get; set; }
+    }
+}

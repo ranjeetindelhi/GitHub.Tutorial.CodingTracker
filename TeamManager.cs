b8409664f5238e4b617f333849efa8d124332557
ranjeetindelhi <116708246+ranjeetindelhi@users.noreply.github.com>
2025-02-28 16:48:14 +0530

    Included Manager class (#2)

---
+++ TeamManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub.Tutorial.CodingTracker
+{
+    public class TeamManager
+    {
+        public int TeamManagerId { get; set; }
+
+        public int TeamId { get; set; }
+
+        public string ManagerName { get; set; }
+    }
+}
